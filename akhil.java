48ca8d0c48d8105e4a3978e65c9d672fbda9f3b6
kousik4416 <krishnakousikb@gmail.com>
2024-09-18 12:26:12 +0530

    Add files via upload

---
+++ akhil.java
@@ -0,0 +1,16 @@
+class person{
+int pid;
+String Name;
+public void getdata();
+{}
+public void display(){}
+}
+class testperson{
+public static void main(string args[]){
+person p1,p2;
+//P1.PID=100
+//p1.Name="sai";
+//p2.pid=101;
+//p2.Name="Lavanya";
+}
+}
